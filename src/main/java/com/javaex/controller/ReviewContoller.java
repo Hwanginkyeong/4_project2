fa3e830fd81b55fce78cb06e0d8375d267d99fe7
hwanginkyeong <inke0831@naver.com>
2022-03-10 23:33:43 +0900

    review

---
+++ src/main/java/com/javaex/controller/ReviewContoller.java
@@ -0,0 +1,12 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/review")
+public class ReviewContoller {
+
+	
+
+}
